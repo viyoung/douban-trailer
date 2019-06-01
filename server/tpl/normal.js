eaa810b6ff4c4829385a946a92cf511ccfb2907a
危阳阳 <viyoung@192.168.0.7>
2019-06-01 21:09:52 +0800

    feat:新增了HTML静态模板

---
+++ server/tpl/normal.js
@@ -0,0 +1,27 @@
+module.exports = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class=col-md-8>
+                <h1>Hi koa2</h1>
+            </div>
+            <div class="col-md-4">
+                <h1>测试页面</h1>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+`
