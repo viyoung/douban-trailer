76d952a13a3a87a83dfb4c38691a922d2e9bcf42
危阳阳 <viyoung@192.168.0.7>
2019-06-02 12:21:40 +0800

    feat:增加koa-view

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const Koa = require('koa');
 const app = new Koa();
-const { htmlTpl, ejsTpl, pugTpl } = require('./tpl/index');
-const ejs = require('ejs');
-const pug = require('pug');
+const views = require('koa-views');
+const { resolve } = require('path');
+app.use(views(resolve(__dirname, './views'), {
+    extension: 'pug'
+}))
 app.use(async (ctx, next) => {
-    ctx.type = 'text/html;charset=utf-8';
-    ctx.body = pug.render(pugTpl, {
+    await ctx.render('index', {
         you: 'koa',
-        me: 'viyoung'
-    });
+        me: '模板引擎'
+    })
 })
 app.listen(4455); 
